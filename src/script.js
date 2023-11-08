67d9b3b8319ff4c7e913f26ec11d0f27db3f2c71
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2023-11-07 21:26:43 -0300

    entregável 1 Luiz Azevedo

--- src/script.js
+++ src/script.js
@@ -1,7 +1,39 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(n1, n2, operador) {
+  let resultado
+  let num1 = parseInt(n1)
+  let num2 = parseInt(n2)
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      let contador = 1
+      resultado = num1 * num1
+      while (contador < num2 - 1) {
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  }
+
+  if (resultado == undefined || resultado > 1000000){
+    resultado = "ERRO"
+  }
+
+  return resultado
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
